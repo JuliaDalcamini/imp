13be646ae36fa44bc0630599fb50cf653e60bde0
Julia Dalcamini <jvdalcamini@gmail.com>
2024-09-18 23:33:14 -0300

    fix: remove login mock

--- composeApp/src/commonMain/kotlin/com/julia/imp/login/LoginUiState.kt
+++ composeApp/src/commonMain/kotlin/com/julia/imp/login/LoginUiState.kt
@@ -3,8 +3,8 @@ package com.julia.imp.login
 import com.julia.imp.common.session.UserSession
 
 data class LoginUiState(
-    val email: String = "tokyodalcamini@gmail.com",
-    val password: String = "123456",
+    val email: String = "",
+    val password: String = "",
     val showError: Boolean = false,
     val loading: Boolean = false,
     val newSession: UserSession? = null
